718b3202a4dd249ac5c6ff134946e3bfb08d3fbc
Gema <gema.ros.t@gmail.com>
2025-08-01 12:40:06 +0200

    Add files via upload

---
+++ 001-simpleTranslator.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from langserve import add_routes
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv, find_dotenv
+
+import os
+
+print("Ruta .env encontrada:", find_dotenv())  # 🔍 Verifica si .env se encuentra
+_ = load_dotenv(find_dotenv())
+print("API KEY cargada:", os.getenv("OPENAI_API_KEY"))  # 🔍 Verifica que se haya leído
+
+openai_api_key = os.environ["OPENAI_API_KEY"]
+
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+
+parser = StrOutputParser()
+
+system_template = "Translate the following into {language}:"
+
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '{text}')
+])
+
+chain = prompt_template | llm | parser
+
+app = FastAPI(
+  title="simpleTranslator",
+  version="1.0",
+  description="A simple API server using LangChain's Runnable interfaces",
+)
+
+add_routes(
+    app,
+    chain,
+    path="/chain",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
